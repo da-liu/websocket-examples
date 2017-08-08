d5e5cac47234b9cbbe3d2617c229697acdb69027
DL <da@daliu.ca>
2017-08-08 17:07:10 -0400

    comments for basic client server pair

--- api/server.js
+++ api/server.js
@@ -1,11 +1,24 @@
+// WebSocket is the name of a protocol for client-server communication
+// E.g. http is a protocol, http://localhost:3000; for WebSocket, ws://localhost:3000
+// 'ws' is one of many libraries that implement an API for the WebSocket protocol
+// Other libraries include: 'socket.io', 'websocket-node', 'primus', etc
+
 var WebSocket = require('ws')
+
+// webSockets extends the EventEmitter class
+// Attaching listeners to this is essentially how server handles WebSocket connections from multiple clients
 var webSockets = new WebSocket.Server({ port: 4002 })
 
+// If a client connects to the server via WebSocket
+// Callback receive a single client connection
 webSockets.on('connection', function(oneSocket) {
 
+  // If the client sends a message, the 'message' event is triggered
   oneSocket.on('message', function(clientMessage) {
 
     console.log(clientMessage)
+    // Here we echo back to the client whatever message was received
+    // Note this only sends to a single client
     oneSocket.send(`Server echo: ${clientMessage}`)
 
   })
